8f7ff1d213f18a0b07e48e67beac8f230c964d85
Anna Mikhailova <Anna.Mikhailova@adenza.com>
2023-06-16 15:54:32 +0400

    move rateLimiter to separated file

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const rateLimiter = require('./utils/rate-limiter');
 const router = require('./routes');
 const errorsHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -13,13 +13,6 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
 
 const { DB_ADDRESS = 'mongodb://0.0.0.0:27017/bitfilmsdb', PORT = 3000 } = process.env;
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
 
 const app = express();
 app.use(cors({
@@ -35,7 +28,7 @@ app.use(cors({
 }));
 app.use(helmet());
 mongoose.connect(DB_ADDRESS);
-app.use(limiter);
+app.use(rateLimiter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
